d600b011833671b441a25e921fcbc563ae0ffebe
davidlin529 <davidlin529@gmail.com>
2017-07-13 22:45:11 +0800

    delete

--- hw7/SymTab.java
+++
@@ -1,9 +0,0 @@
-
-/* DO NOT CHANGE:  This file is used in evaluation */
-
-public class SymTab<Whatever extends Base> extends Tree<Whatever> {
-
-        public SymTab (String name){
-                super (name);
-        }
-}
