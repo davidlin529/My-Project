6c11620ddcdbe2ab0c1a8c8c1105ee6438f1e9b1
davidlin529 <davidlin529@gmail.com>
2017-07-13 22:38:21 +0800

    delete

--- hw6/Base.java
+++
@@ -1,9 +0,0 @@
-/* DO NOT CHANGE:  This file is used in evaluation */
-
-public abstract class Base {
-
-        public String getName () {              // identifier function
-
-                return null;
-        }               
-}
--- hw6/SymTab.java
+++
@@ -1,7 +0,0 @@
-/* DO NOT CHANGE:  This file is used in evaluation */
-
-public class SymTab extends HashTable {
-        public SymTab (int sz) {
-                super (sz);
-        }
-}
